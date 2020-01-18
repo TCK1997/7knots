317dd2dbf2713f1abb26574a5792d7015354dbae
rssujay <rssujay97@gmail.com>
2020-01-18 20:01:53 +0800

    Make background responsive

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
+const screenHeight = window.innerHeight;
+const screenWidth = window.innerWidth;
+
 var config = {
         type: Phaser.AUTO,
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: screenWidth,
+        height: screenHeight,
         scene: {
             preload,
             create,
@@ -12,11 +15,13 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('bg', '../assets/introScreen.jpg');
+    this.load.image('bg', '../assets/sky.png');
+    this.load.image('bg2', '../assets/introScreen.jpg');
 }
 
 function create() {
-    this.add.image(0, 0, 'bg').setOrigin(0).setScale(4.5,3);
+    this.add.image(0, 0, 'bg').setOrigin(0).setScale( screenWidth / 800, screenHeight / 600);
+    this.add.image(0, 0, 'bg2').setOrigin(0).setAlpha(0.25).setScale(screenWidth/344, screenHeight/256);
 }
 
 function update() {
