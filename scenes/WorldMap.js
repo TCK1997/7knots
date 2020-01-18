8d5ae5b1ca257e81a5a19b2d5336cb38c14188f5
TCK1997 <achinkhang97.sg@gmail.com>
2020-01-19 05:42:59 +0800

    add setter for location and state change

--- scenes/WorldMap.js
+++ scenes/WorldMap.js
@@ -7,7 +7,6 @@ class WorldMap extends Phaser.Scene {
     
     constructor() {
         super("WorldMap");
-        
     }
 
     preload() {
@@ -20,6 +19,8 @@ class WorldMap extends Phaser.Scene {
     }
     
     create() {
+        var currentLocation;
+        var futureLocation;
         curve = new Phaser.Curves.Line(new Phaser.Math.Vector2(160, 91), new Phaser.Math.Vector2(1726, 286));
 
         this.add.image(0, 0, 'gameMap').setOrigin(0).setScale( screenWidth / 1326, screenHeight / 1013);
@@ -27,25 +28,57 @@ class WorldMap extends Phaser.Scene {
         var gameMapEvent = this.add.image(0, 0, 'gameMap').setOrigin(0).setScale( screenWidth / 1326, screenHeight / 1013).setInteractive().on('pointerdown', function (pointer) {
             if ((pointer.x < 95 && pointer.y < 240) && (pointer.y > 200)) {
                 console.log(1)
+                currentLocation = getLocation();
+                futureLocation = 1;
+                setLocation(1);
+
                 this.scene.scene.start("Port") //1. Cidade
+                
             } else if ((pointer.x < screenWidth/980*437 && pointer.y < screenHeight/816*460) && (pointer.x > screenWidth/980*360 && pointer.y > screenHeight/816*437)) {
                 console.log(2)
+                currentLocation = getLocation();
+                futureLocation = 2;
+                setLocation(2);
                 this.scene.scene.start("Port") //2. Mombasa
             } else if ((pointer.x < screenWidth/980*567 && pointer.y < screenHeight/816*343) && (pointer.x > screenWidth/980*500 && pointer.y > screenHeight/816*322)) {
+                currentLocation = getLocation();
+                futureLocation = 3;
+                setLocation(3);
                 this.scene.scene.start("Port") //3. Socotra
             } else if ((pointer.x < screenWidth/980*660 && pointer.y < screenHeight/816*340) && (pointer.x > screenWidth/980*614 && pointer.y > screenHeight/816*312)) {
+                currentLocation = getLocation();
+                futureLocation = 4;
+                setLocation(4);
                 this.scene.scene.start("Port") //4. Goa
             } else if ((pointer.x < screenWidth/980*670 && pointer.y < screenHeight/816*368) && (pointer.x > screenWidth/980*610 && pointer.y > screenHeight/816*345)) {
+                currentLocation = getLocation();
+                futureLocation = 5;
+                setLocation(5);
                 this.scene.scene.start("Port") //5. Calicut
             } else if ((pointer.x < screenWidth/980*708 && pointer.y < screenHeight/816*410) && (pointer.x > screenWidth/980*656 && pointer.y > screenHeight/816*374)) {
+                currentLocation = getLocation();
+                futureLocation = 6;
+                setLocation(6);
                 this.scene.scene.start("Port") //6. Colombo
             } else if ((pointer.x < screenWidth/980*890 && pointer.y < screenHeight/816*424) && (pointer.x > screenWidth/980*824 && pointer.y > screenHeight/816*406)) {
+                currentLocation = getLocation();
+                futureLocation = 7;
+                setLocation(7);
                 this.scene.scene.start("Port") //7. Malacca
             } else if ((pointer.x < screenWidth/980*974 && pointer.y < screenHeight/816*513) && (pointer.x > screenWidth/980*916 && pointer.y > screenHeight/816*479)) {
+                currentLocation = getLocation();
+                futureLocation = 8;
+                setLocation(8);
                 this.scene.scene.start("Port") //8. Makassar
             } else if ((pointer.x < screenWidth/980*935 && pointer.y < screenHeight/816*239) && (pointer.x > screenWidth/980*890 && pointer.y > screenHeight/816*200)) {
+                currentLocation = getLocation();
+                futureLocation = 9;
+                setLocation(9);
                 this.scene.scene.start("Port") //9. Canton
             } else if ((pointer.x < screenWidth/980*130 && pointer.y < screenHeight/816*85) && (pointer.x > screenWidth/980*75 && pointer.y > screenHeight/816*67)) {
+                currentLocation = getLocation();
+                futureLocation = 0;
+                setLocation(0);
                 this.scene.scene.start("Port") //0. Lisbon
             }
         })
