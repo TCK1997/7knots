a92884836a327ad4d35f0fb74c9b9b6354d3be01
jwyf <e0311252@u.nus.edu>
2020-01-18 22:24:10 +0800

    Add interactive buttons to MainMenu and Introduction

--- scenes/Introduction.js
+++ scenes/Introduction.js
@@ -4,16 +4,28 @@ class Introduction extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.image('backBtn', '../assets/buttons/backBtn.png')
     }
     
     create() {
 
         var font = { font: '24px Arial' };
 
-        var text = this.add.text(60, 60, "INTRODUCTION STARTS HERE", font).setInteractive().on('pointerdown', function (pointer) {
+        var text = this.add.text(60, 60, "INTRODUCTION STARTS HERE", font)
+
+        //Back button
+        var backEvent = this.add.image(screenWidth*0.1, screenHeight*0.85, "backBtn", font).setInteractive().on('pointerdown', function (pointer) {
             this.scene.scene.start("MainMenu")
         })
+
+        backEvent.on('pointerover',function(pointer){
+            backEvent.setAlpha(0.75);
+        })
+
+        backEvent.on('pointerout',function(pointer){
+            backEvent.setAlpha(1);
+        })
+        
     }
     
     update() {
--- scenes/MainMenu.js
+++ scenes/MainMenu.js
@@ -6,6 +6,8 @@ class MainMenu extends Phaser.Scene {
     preload() {
         this.load.image('bg', '../assets/sky.png');
         this.load.image('bg2', '../assets/introScreen.jpg');
+        this.load.image('playBtn', '../assets/buttons/playBtn.png')
+        this.load.image('continueBtn', '../assets/buttons/continueBtn.png')
     }
     
     create() {
@@ -14,12 +16,36 @@ class MainMenu extends Phaser.Scene {
 
         this.add.image(0, 0, 'bg2').setOrigin(0).setAlpha(0.25).setScale(screenWidth/344, screenHeight/256);
 
-        var font = { font: '24px Arial' };
+        //Game Name: 7Knots
+        var font = { font: '50px Arial' };
 
-        var text = this.add.text(60, 60, "START", font).setInteractive().on('pointerdown', function (pointer) {
+        var gameName = this.add.text(screenWidth*0.2,screenHeight*0.3 , "7 Knots", font)
+
+        //Play button
+        var playEvent = this.add.image(screenWidth*0.8, screenHeight*0.35, "playBtn", font).setInteractive().on('pointerdown', function (pointer) {
             this.scene.scene.start("Introduction")
         })
-        
+
+        playEvent.on('pointerover',function(pointer){
+            playEvent.setAlpha(0.75);
+        })
+
+        playEvent.on('pointerout',function(pointer){
+            playEvent.setAlpha(1);
+        })
+
+        //Continue button
+        var continueEvent = this.add.image(screenWidth*0.8, screenHeight*0.44, "continueBtn", font).setInteractive().on('pointerdown', function (pointer) {
+            //Load saved game, continue from previous save
+        })
+
+        continueEvent.on('pointerover',function(pointer){
+            continueEvent.setAlpha(0.75);
+        })
+
+        continueEvent.on('pointerout',function(pointer){
+            continueEvent.setAlpha(1);
+        })
     }
 
     update() {
