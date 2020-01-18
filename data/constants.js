882672fda6ee05add66dd8c049b60863a51a7eae
Rohan Arya Varma <rohanaryavarma@gmail.com>
2020-01-18 20:57:00 +0800

    Add data folder containing static data and dynamic game state data

---
+++ data/constants.js
@@ -0,0 +1,1 @@
+// Hard-coded constants used for the game (i.e. for cities, names, resource demand and supply etc.)
---
+++ data/state.js
@@ -0,0 +1,1 @@
+// Game State: Morale, 3 Modifiers (NAV, MTV, CHA), Resources, Gold, Location, Crew, Date (Day)
