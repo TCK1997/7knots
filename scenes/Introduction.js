3bb2b3d9317ca4db48971f64abf27805d74a046c
Rohan Arya Varma <rohanaryavarma@gmail.com>
2020-01-19 10:05:18 +0800

    Finish introduction

--- scenes/Introduction.js
+++ scenes/Introduction.js
@@ -1,48 +1,55 @@
 class Introduction extends Phaser.Scene {
-    constructor() {
-        super("Introduction");
-    }
+  constructor() {
+    super("Introduction");
+  }
 
-    preload() {
-        this.load.image('backBtn', '../assets/buttons/backBtn.png')
-        this.load.image('nextBtn', '../assets/buttons/nextBtn.png')
-    }
-    
-    create() {
+  preload() {
+    this.load.image("backBtn", "../assets/buttons/backBtn.png");
+    this.load.image("nextBtn", "../assets/buttons/nextBtn.png");
+  }
 
-        var font = { font: '24px Arial' };
+  create() {
+    var font = { font: "24px Arial" };
 
-        var text = this.add.text(60, 60, "Born to a family of successful sailors, your childhood was ", font)
+    var style = {
+      font: "24px Arial",
+      wordWrap: { width: screenWidth * 0.8, useAdvancedWrap: true }
+    };
 
-        //Back button
-        var backEvent = this.add.image(screenWidth*0.08, screenHeight*0.92, "backBtn", font).setInteractive().on('pointerdown', function (pointer) {
-            this.scene.scene.start("MainMenu")
-        })
+    var text = this.add.text(60, 60, introductionText, style);
 
-        backEvent.on('pointerover',function(pointer){
-            backEvent.setAlpha(0.75);
-        })
+    //Back button
+    var backEvent = this.add
+      .image(screenWidth * 0.08, screenHeight * 0.92, "backBtn", font)
+      .setInteractive()
+      .on("pointerdown", function(pointer) {
+        this.scene.scene.start("MainMenu");
+      });
 
-        backEvent.on('pointerout',function(pointer){
-            backEvent.setAlpha(1);
-        })
+    backEvent.on("pointerover", function(pointer) {
+      backEvent.setAlpha(0.75);
+    });
 
-        //Next button
-        var nextEvent = this.add.image(screenWidth*0.9, screenHeight*0.92, "nextBtn", font).setInteractive().on('pointerdown', function (pointer) {
-            this.scene.scene.start("WorldMap")
-        })
+    backEvent.on("pointerout", function(pointer) {
+      backEvent.setAlpha(1);
+    });
 
-        nextEvent.on('pointerover',function(pointer){
-            nextEvent.setAlpha(0.75);
-        })
+    //Next button
+    var nextEvent = this.add
+      .image(screenWidth * 0.9, screenHeight * 0.92, "nextBtn", font)
+      .setInteractive()
+      .on("pointerdown", function(pointer) {
+        this.scene.scene.start("WorldMap");
+      });
 
-        nextEvent.on('pointerout',function(pointer){
-            nextEvent.setAlpha(1);
-        })
-        
-    }
-    
-    update() {
-    
-    }
+    nextEvent.on("pointerover", function(pointer) {
+      nextEvent.setAlpha(0.75);
+    });
+
+    nextEvent.on("pointerout", function(pointer) {
+      nextEvent.setAlpha(1);
+    });
+  }
+
+  update() {}
 }
